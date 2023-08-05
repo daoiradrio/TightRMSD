3dbfdb0eeabcb20479dafd7f7ffdd7b0d4487be6
Dario Baum <dario-cbr@hotmail.de>
2023-08-05 18:38:51 +0200

    Refactoring

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,10 +5,10 @@
 
 
 
-extern std::unordered_map<std::string, int> element_numbers;
-extern std::unordered_map<std::string, double> valence_radii_single;
-extern std::unordered_map<std::string, double> valence_radii_double;
-extern std::unordered_map<std::string, double> valence_radii_triple;
+extern std::unordered_map<std::string, int>     element_numbers;
+extern std::unordered_map<std::string, double>  valence_radii_single;
+extern std::unordered_map<std::string, double>  valence_radii_double;
+extern std::unordered_map<std::string, double>  valence_radii_triple;
 
 
 
